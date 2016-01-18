aa51c97fb942ec2d2989cc6b4253336a113d1751
Benjamin MOREAU <E125757S@S046V7pc12.ensinfo.sciences.univ-nantes.prive>
2016-01-18 13:52:47 +0100

    G0 added

---
+++ grammaire.c
@@ -0,0 +1,28 @@
+void genStar(void*(*codeTache)(void*)){
+
+}
+
+void genUn(void*(*codeTache)(void*)){
+
+}
+
+void genAtom(void*(*codeTache)(void*)){
+
+}
+
+void genUnion(void*(*codeTache)(void*)){
+
+}
+
+void genForet(void*(*codeTache)(void*)){
+
+}
+
+void ImprimArbre(void*(*codeTache)(void*)){
+
+}
+
+int main(){
+
+	return 0;
+}
