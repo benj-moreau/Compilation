2bc357daf50cfe6f7b49b81b367c559e85f8a436
benjimor <benji_moreau@hotmail.fr>
2016-01-20 13:54:20 +0100

    g0 changed

--- grammaire.c
+++ grammaire.c
@@ -1,43 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 struct nodeType
 {
     nodeType *left;
     nodeType *right;
-    char class[10];
+    char class[5];
 };
 
 nodeType * genConc(nodeType * p1, nodeType * p2){
-	nodeType node;
-	node.left = p1;
-	node.right = p2;
-	node.class = "conc";
-	return (&node);
+	nodeType * node;
+	node->left = p1;
+	node->right = p2;
+	node->class[] = "conc";
+	return (node);
 }
 
 nodeType * genUnion(nodeType * p1, nodeType * p2){
-	nodeType node;
-	node.left = p1;
-	node.right = p2;
-	node.class = "union";
-	return (&node);
+	nodeType * node;
+	node->left = p1;
+	node->right = p2;
+	node->class[] = "union";
+	return (node);
 }
 
 nodeType * genStar(nodeType * p1){
-	nodeType node;
-	node.left = p1;
-	node.class = "star";
-	return (&node);
+	nodeType * node;
+	node->left = p1;
+	node->class[] = "star";
+	return (node);
 }
 
 nodeType * genUn(nodeType * p1){
-	nodeType node;
-	node.left = p1;
-	node.class = "un";
-	return (&node);
+	nodeType * node;
+	node->left = p1;
+	node->class[] = "un";
+	return (node);
 }
 
 nodeType * genAtom(){
-	nodeType node;
-	return (&node)
+	nodeType * node;
+	return (node);
 }
 
 nodeType ** genForet(){
@@ -50,6 +54,6 @@ void ImprimArbre(nodeType *root, int prof = 1){
 }
 
 int main(){
-	
+
 	return 0;
 }
