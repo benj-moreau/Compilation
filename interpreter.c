9f4df6b47e1a9ca29da3449023c0fee8382b2a25
dennisman <dennisman@laposte.net>
2016-03-25 12:44:12 +0100

    debut interpreter

--- interpreter.c
+++ interpreter.c
@@ -1,16 +1,16 @@
 
-char **pilex;
+int *pilex;
 int spx;
 
-char **pcode;
-int co;
+int* pcode;
+int c0;
 
 void init(){
-	pilex = malloc(100*sizeof(char*));
+	pilex = malloc(100*sizeof(int));
 	spx=0;
 
-	pcode = malloc(100*sizeof(char*));
-	co=0;
+	pcode = malloc(100*sizeof(int));
+	c0=0;
 
 }
 
@@ -51,73 +51,130 @@ INDV
 
 void interpreter(int instr){
 	switch (instr){
-	case LDA:
+	case LDA:{
+		spx++;
+		pilex[spx] = pcode[c0+1]; // on met dans la pile d'execution la valeur suivante du code
+		c0+=2;
+	}break;
+	case LDV:{
+		spx++;
+		pilex[spx] = pilex[pcode[c0+1]];// on met dans la pile d'execution la valeur de la variable en parametre dans le code apres ldv
+		c0+=2;
+	}break;
+	case LDC:{
+		spx++;
+		pilex[spx] = pcode[c0+1];// on met dans la pile d'execution la valeur suivante du code
+		c0+=2;
+    }break;
+	case JMP:{
+		c0=pcode[c0+1];
+    }break;
+	case JIF:{
+		if(pilex[spx]==0){
+			c0=pcode[c0+1];
+		}else{
+			c0+=2;
+		}
+    }break;
+	case JSR:{
+    }break;
+	case RSR:{
+    }break;
+	case SUP:{
+		spx--; // on descend car apres la comparaison, on pourra écraser une des 2 valeurs et ne garder que le booleen correspondant
+		if(pilex[spx]>pilex[spx+1]){
+			pilex[spx] = 1;
+		}else{
+			pilex[spx] = 0;
+		}
+		c0++;
+    }break;
+	case SUPE:{
+		spx--; 
+		if(pilex[spx] >= pilex[spx+1]){
+			pilex[spx] = 1;
+		}else{
+			pilex[spx] = 0;
+		}
+		c0++;
+    }break;
+	case INF:{
+		spx--; 
+		if(pilex[spx] < pilex[spx+1]){
+			pilex[spx] = 1;
+		}else{
+			pilex[spx] = 0;
+		}
+		c0++;
+    }break;
+	case INFE:{
+		spx--; 
+		if(pilex[spx] <= pilex[spx+1]){
+			pilex[spx] = 1;
+		}else{
+			pilex[spx] = 0;
+		}
+		c0++;
+    }break;
+	case EG:{
+		spx--; 
+		if(pilex[spx] == pilex[spx+1]){
+			pilex[spx] = 1;
+		}else{
+			pilex[spx] = 0;
+		}
+		c0++;
+    }break;
+	case DIFF:{
+		spx--; 
+		if(pilex[spx] != pilex[spx+1]){
+			pilex[spx] = 1;
+		}else{
+			pilex[spx] = 0;
+		}
+		c0++;
+    }break;
+	case RD:{
+    }break;
+	case RDLN:{
+    }break;
+	case WRT:{
+    }break;
+	case WRTLN:{
+    }break;
+	case ADD:{
+    }break;
+	case MOINS:{
+    }break;
+	case DIV:{
+    }break;
+	case NEG:{
+    }break;
+	case MULT:{
+    }break;
+	case INC:{
+    }break;
+	case DEC:{
+    }break;
+	case AND:{
+    }break;
+	case OR:{
+    }break;
+	case NOT:{
+    }break;
+	case AFF:{
 	
-	break;
-	case LDV:
-	break;
-	case LDC:
+    }break;
+	case STOP:{
 	
-    break;
-	case JMP:
-    break;
-	case JIF:
-    break;
-	case JSR:
-    break;
-	case RSR:
-    break;
-	case SUP:
-    break;
-	case SUPE:
-    break;
-	case INF:
-    break;
-	case INFE:
-    break;
-	case EG:
-    break;
-	case DIFF:
-    break;
-	case RD:
-    break;
-	case RDLN:
-    break;
-	case WRT:
-    break;
-	case WRTLN:
-    break;
-	case ADD:
-    break;
-	case MOINS:
-    break;
-	case DIV:
-    break;
-	case NEG:
-    break;
-	case MULT:
-    break;
-	case INC:
-    break;
-	case DEC:
-    break;
-	case AND:
-    break;
-	case OR:
-    break;
-	case NOT:
-    break;
-	case AFF:
+    }break;
+	case INDA:{
+    }break;
+	case INDV:{
+	}break;
+	default:{
+	}break;
 	
-    break;
-	case STOP:
-	
-    break;
-	case INDA:
-    break;
-	case INDV:
-	default:
-	break;
 	}
 }
 int main() {
